ea92591013ada269b1f5dbea0e9843706b980cf4
danilloguimaraes <danillo@danilloguimaraes.com.br>
2014-09-30 18:01:26 -0300

    Adequação ao padrão da Fábrica de Software

--- central/src/main/java/br/ufg/inf/fs/muralufg/central/identificacao/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Implementação da funcionalidade de identificação
- * da Central UFG.
- */
-package br.ufg.inf.fs.muralufg.central.identificacao;
--- central/src/main/java/br/ufg/inf/fs/muralufg/central/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Package "raiz" do Componente Central do Projeto Mural-UFG.
- * Package das classes de inicialização e configuração da Central.
- */
-package br.ufg.inf.fs.muralufg.central;
